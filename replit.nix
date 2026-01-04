2cc94c5d260d46bb61caaf96a623a5df04b9745d
sayedelazameydesign-crypto <sayedelazameydesign@gmail.com>
2026-01-04 14:45:13 +0200

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -1,8 +1,16 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodejs-20_x
-    pkgs.nodePackages.typescript-language-server
-    pkgs.yarn
-    pkgs.replitPackages.jest
-  ];
-}
+run = "npx concurrently \"npm run dev\" \"npm run server\""
+entrypoint = "index.tsx"
+
+[modules]
+modules = ["nodejs-20"]
+
+[nix]
+channel = "stable-24_05"
+
+[deployment]
+run = ["npm", "run", "build"]
+
+ports = [
+  { localPort = 3000, externalPort = 80 },
+  { localPort = 5000, externalPort = 5000 }
+]
